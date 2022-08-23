6b81a58593adcb89ba5a8ad2b2d91a76d968d5c8
Bright moon <thinhpk26@gmail.com>
2022-08-23 22:52:36 +0700

    fix bug find room

--- router/room.router.js
+++ router/room.router.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middleware/auth.middleware');
 const roomRouter = express.Router();
 
 roomRouter.get('/basic-infor', authMiddleware, getInforBasicRoom)
-roomRouter.post('/get-all-room-of-user', getAllRoomOfUser)
+roomRouter.post('/get-all-room-of-user', postMiddleware, getAllRoomOfUser)
 roomRouter.post('/send-request', postMiddleware, postRequestIntoRoom)
 roomRouter.get('/request-room', authMiddleware, getRequestIntoRoom)
 roomRouter.put('/confirm-request', authMiddleware, putConfirm)
